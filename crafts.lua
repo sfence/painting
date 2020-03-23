f90b3a46da3fd75d543ff93366c8348c8966b0a0
wuniversales <48658161+wuniversales@users.noreply.github.com>
2020-03-23 21:06:02 +0100

    Added compatibility with Mineclone 2

--- crafts.lua
+++ crafts.lua
@@ -5,35 +5,68 @@
 --    UNDER TERMS OF WTFPL LICENSE
 
 -- 2012, 2013, 2014 obneq aka jin xi
+if minetest.get_modpath("mcl_core") then
+    minetest.register_craft({
+        output = 'painting:easel 1',
+        recipe = {
+            { '', 'mcl_core:wood', '' },
+            { '', 'mcl_core:wood', '' },
+            { 'mcl_core:stick','', 'mcl_core:stick' },
+        }})
 
-minetest.register_craft({
-      output = 'painting:easel 1',
-      recipe = {
-         { '', 'default:wood', '' },
-         { '', 'default:wood', '' },
-         { 'default:stick','', 'default:stick' },
-      }})
+    minetest.register_craft({
+        output = 'painting:canvas_16 1',
+        recipe = {
+            { '', '', '' },
+            { '', '', '' },
+            { 'mcl_core:paper', '', '' },
+        }})
 
-minetest.register_craft({
-      output = 'painting:canvas_16 1',
-      recipe = {
-         { '', '', '' },
-         { '', '', '' },
-         { 'default:paper', '', '' },
-      }})
+    minetest.register_craft({
+        output = 'painting:canvas_32 1',
+        recipe = {
+            { '', '', '' },
+            { 'mcl_core:paper', 'mcl_core:paper', '' },
+            { 'mcl_core:paper', 'mcl_core:paper', '' },
+        }})
 
-minetest.register_craft({
-      output = 'painting:canvas_32 1',
-      recipe = {
-         { '', '', '' },
-         { 'default:paper', 'default:paper', '' },
-         { 'default:paper', 'default:paper', '' },
-      }})
+    minetest.register_craft({
+        output = 'painting:canvas_64 1',
+        recipe = {
+            { 'mcl_core:paper', 'mcl_core:paper', 'mcl_core:paper' },
+            { 'mcl_core:paper', 'mcl_core:paper', 'mcl_core:paper' },
+            { 'mcl_core:paper', 'mcl_core:paper', 'mcl_core:paper' },
+        }})
+else
+    minetest.register_craft({
+        output = 'painting:easel 1',
+        recipe = {
+            { '', 'default:wood', '' },
+            { '', 'default:wood', '' },
+            { 'default:stick','', 'default:stick' },
+        }})
 
-minetest.register_craft({
-      output = 'painting:canvas_64 1',
-      recipe = {
-         { 'default:paper', 'default:paper', 'default:paper' },
-         { 'default:paper', 'default:paper', 'default:paper' },
-         { 'default:paper', 'default:paper', 'default:paper' },
-      }})
+    minetest.register_craft({
+        output = 'painting:canvas_16 1',
+        recipe = {
+            { '', '', '' },
+            { '', '', '' },
+            { 'default:paper', '', '' },
+        }})
+
+    minetest.register_craft({
+        output = 'painting:canvas_32 1',
+        recipe = {
+            { '', '', '' },
+            { 'default:paper', 'default:paper', '' },
+            { 'default:paper', 'default:paper', '' },
+        }})
+
+    minetest.register_craft({
+        output = 'painting:canvas_64 1',
+        recipe = {
+            { 'default:paper', 'default:paper', 'default:paper' },
+            { 'default:paper', 'default:paper', 'default:paper' },
+            { 'default:paper', 'default:paper', 'default:paper' },
+        }})
+end
