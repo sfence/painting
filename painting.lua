8f4db2a8bb751be8778b8eb3d31a2d3531b2b214
ssdaniel24 <bo7oaonteg2m@mail.ru>
2024-06-10 20:26:52 +0300

    Added missing localization wrappers

--- painting.lua
+++ painting.lua
@@ -533,14 +533,14 @@ local textures = {
 }
 
 minetest.register_craftitem("painting:brush", {
-		description = "Brush",
+		description = S("Brush"),
 		inventory_image = "painting_brush_stem.png^(painting_brush_head.png^[colorize:#FFFFFF:128)^painting_brush_head.png",
 	})
 
 local vage_revcolours = {} -- ← colours in pairs order
 for color, _ in pairs(textures) do
 	local brush_new = table.copy(brush)
-	brush_new.description = color:gsub("^%l", string.upper).." brush"
+	brush_new.description = S(color:gsub("^%l", string.upper).." brush")
 	brush_new.inventory_image = "painting_brush_stem.png^(painting_brush_head.png^[colorize:#"..hexcolors[color]..":255)^painting_brush_head.png"
 	brush_new._painting_brush.color = hexcolors[color]
 	minetest.register_tool("painting:brush_"..color, brush_new)
