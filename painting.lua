aa2f7a0f2b0e2d90722a39959401983e3d0a5191
blanka kato <112077355+blanka-kato@users.noreply.github.com>
2025-03-23 01:39:52 +0500

    Consume one canvas per use. Fix #16

--- painting.lua
+++ painting.lua
@@ -500,19 +500,22 @@ core.register_node("painting:easel", {
 			ent.res = data.res
 			ent.version = data.version
 			obj:set_properties{textures = { painting.to_imagestring(ent.grid, ent.res) }}
-			player:get_inventory():remove_item("main", wield_item:take_item())
 		else
 			ent.grid = initgrid(def._painting_canvas_resolution)
 			ent.res = def._painting_canvas_resolution
 			ent.version = current_version
-			if not core.is_creative_enabled(player:get_player_name()) then
-				player:get_inventory():remove_item("main", wield_item:take_item())
-			end
 		end
 		ent.fd = fd
 
 		meta:set_int("has_canvas", 1)
-		player:get_inventory():set_stack("main", wield_item_idx, ItemStack(""))
+
+		-- Remove only 1 canvas from the inventory when the player is not in creative mode.
+		if not core.is_creative_enabled(player:get_player_name()) then
+			local inv = player:get_inventory()
+			local current_stack = inv:get_stack("main", wield_item_idx)
+			current_stack:take_item(1)
+			inv:set_stack("main", wield_item_idx, current_stack)
+		end
 	end,
 
 	can_dig = function(pos)
